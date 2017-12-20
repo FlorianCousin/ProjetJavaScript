e6ed2e73891895bd02b7b50c347b1876a9a239c4
Cousin Florian <florian.cousin@telecom-st-etienne.fr>
2017-12-20 16:08:05 +0100

    délectable affichage correct du fond

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ProjetJavaScript
+
+You have to read me before starting to code.
+Indeed, I am very important
--- gameProjet.js
+++ gameProjet.js
@@ -7,8 +7,8 @@ function afficher() {
 	if (!grass.loaded) {
 		return;
 	}
-	for (var i = 0; i < 11; i++) {
-		for (var j = 0; j < 10; j++) {
+	for (var i = 0; i < 10; i++) {
+		for (var j = 0; j < 13; j++) {
 			ctx.drawImage(grass, 64 * i, 64 * j);
 		}
 	}
