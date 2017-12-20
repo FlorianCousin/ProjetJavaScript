e15ae35b442ff535d8a43f5d4fa416b2d72db58e
Cousin Florian <florian.cousin@telecom-st-etienne.fr>
2017-12-20 15:33:05 +0100

    Initial commit

---
+++ gameProjet.js
@@ -0,0 +1,24 @@
+var cs = document.getElementById("cv");
+ctx = cs.getContext("2d");
+
+
+
+function afficher() {
+	if (!grass.loaded) {
+		return;
+	}
+	for (var i = 0; i < 11; i++) {
+		for (var j = 0; j < 10; j++) {
+			ctx.drawImage(grass, 64 * i, 64 * j);
+		}
+	}
+}
+
+
+var grass = new Image();
+grass.loaded = false;
+grass.src = "grass.png";
+grass.onload = function() {
+	grass.loaded = true;
+	afficher();
+};
